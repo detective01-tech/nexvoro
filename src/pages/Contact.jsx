d1b62e855cbea44153cdb0728d158655331e8057
Muhammad Sohail <wearixastore@gmail.com>
2026-08-22 01:21:40 +0500

    new chnages for new ad are done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,9 +9,6 @@ export default function Contact() {
 
   function handleSubmit(e) {
     e.preventDefault()
-    if (window.gtag_report_conversion) {
-      window.gtag_report_conversion()
-    }
     setSubmitted(true)
   }
 
